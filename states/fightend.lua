9cab63401840780d1a1d016f4537ea2a51bbebdd
Darren Royle <darren.royle@oup.com>
2014-07-10 20:23:19 +0100

    Added the end fight state

---
+++ states/fightend.lua
@@ -0,0 +1,12 @@
+local AsciiSprite = require('ascii.ascii_sprite')
+local Renderer = require('display.renderer')
+local AvatarTalker = require('display.AvatarTalker')
+local Button = require('display.button')
+
+FightEndState = {}
+
+function FightEndState:init()
+
+end
+
+return FightEndState
