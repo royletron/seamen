690cd30e5d140236cac6fdc75662825e8f64d88b
Darren Royle <darren.royle@oup.com>
2014-07-15 17:21:36 +0100

    Getting info on the fightend

--- states/fightend.lua
+++ states/fightend.lua
@@ -2,10 +2,9 @@ local PlayerShipDisplay = require('display.PlayerShipDisplay')
 
 FightEndState = {}
 
-local ship_renderer = PlayerShipDisplay(300, 100)
-
 function FightEndState:init()
   self.exp = 0
+  self.ship_renderer = PlayerShipDisplay(300, 100)
 end
 
 function FightEndState:enter()
@@ -13,12 +12,13 @@ function FightEndState:enter()
 end
 
 function FightEndState:update(dt)
-
+  self.ship_renderer:update(dt)
 end
 
 function FightEndState:draw(dt)
+  self.ship_renderer:draw(dt)
   love.graphics.setColor(255,255,255,255)
-  love.graphics.printf(player.ship.name .. ' lv' .. player.level, 100, 290, 200, "center")
+  love.graphics.printf(player.ship.name .. ' lv' .. player.level, self.ship_renderer.x, self.ship_renderer.y + self.ship_renderer.height + 10, self.ship_renderer.width, "center")
 end
 
 return FightEndState
